1b010a9faa00021fcabc18e14d73dab9e84a4378
manudurgoni <emmanuel@carillon.studio>
2021-10-19 11:19:03 +0200

    update

--- swiper/app.js
+++ swiper/app.js
@@ -4,16 +4,31 @@ const init = () => {
 
   const nbCards = cards.length - 1
   cards.forEach((card, i) => {
-    // 10, c'est l'offset entre chaque carte
-    const offset = `${i * 5}px`
-    card.style.setProperty('--offset', offset)
+    // On stock si l'index et pair ou impar
+    const isEven = i % 2 === 0
+    // On créé un multiplicateur positif ou negatif
+    const mult = (isEven ? 1 : -1)
+
+    const spaceX = (0.6 * Math.random() * mult) + 4
+    const spaceY = (Math.random() * mult) + 2
+    
+    const offsetX = `${i * spaceX}px`
+    const offsetY = `${i * spaceY}px`
+    
+    card.style.setProperty('--offsetX', offsetX)
+    card.style.setProperty('--offsetY', offsetY)
 
     const color = `rgb(0, ${255 - (i/30 * 255)}, 0)`
-    console.log(color)
     card.style.setProperty('--color', color)
 
     const z = nbCards - i
     card.style.setProperty('--zIndex', z)
+
+
+    //Rotation
+    const rotationX = (i + 1) * Math.PI * 0.5
+
+    card.style.setProperty('--rotationX', `${rotationX}deg`)
   })
 }
 
