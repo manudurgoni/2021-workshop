d533f72812c698975c07647b96e3c3aecc77181f
manudurgoni <emmanuel@carillon.studio>
2021-10-20 10:32:30 +0200

    add utils

--- api/app.js
+++ api/app.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const timeout = (t, val) => {
+  return new Promise(function(resolve) {
+      setTimeout(function() {
+          resolve(val);
+      }, t);
+  });
+}
+
+
+const getRecommandations = async () => {
+  await timeout(1000)
+  console.log('j’attendu 1s')
+}
+
+getRecommandations()
