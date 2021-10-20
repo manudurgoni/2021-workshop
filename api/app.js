235ff60ccf4b49b76f946c59630d41350d282be1
manudurgoni <emmanuel@carillon.studio>
2021-10-20 11:45:15 +0200

    api append data to dom

--- api/app.js
+++ api/app.js
@@ -1,15 +1,29 @@
 const timeout = (t, val) => {
-  return new Promise(function(resolve) {
-      setTimeout(function() {
-          resolve(val);
-      }, t);
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(val);
+    }, t);
   });
-}
-
+};
 
 const getRecommandations = async () => {
-  await timeout(1000)
-  console.log('j’attendu 1s')
+  const response = await fetch('fakeData.json')
+  const json = await response.json()
+
+  // Afficher le contenu de la propriete 1 dans un H1
+  // Afficher le contenu de la propriete 2 dans un p
+
+  writeText(json)
+  console.log(json)
+};
+
+const writeText = (json) => {
+  const title = document.querySelector('.title')
+  const text = document.querySelector('.text')
+
+  title.innerHTML = json.id
+  text.innerHTML = json.artist
 }
 
-getRecommandations()
+
+getRecommandations();
